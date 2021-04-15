8a1cb64a8245762d43ec753f63841b7f66c4c0a4
Alan R Panosso <arpanosso@yahoo.com.br>
2021-04-14 22:18:01 -0300

    atualização do banco de dados

---
+++ data-raw/tratamento.R
@@ -0,0 +1,6 @@
+# leitura do TXT
+tab <- read.table("data-raw/exemplo01.txt",
+                  h=TRUE)
+head(tab)
+
+usethis::use_github()
--- script_craicao.R
+++ script_craicao.R
@@ -1,4 +1,6 @@
 # usethis::create_project("C:\\ProjetosR\\lab_ciencia_dados")
 
 # Criar a estrutura GIT para esse projeto
-
+# usethis::use_git()
+# usethis::browse_github_token()
+# gitcreds::gitcreds_set()
